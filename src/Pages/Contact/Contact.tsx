f6d150b736f1cd9d08cad5ce542e282df3208a70
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-11-29 11:24:50 -0300

    remove contact page

--- src/Pages/Contact/Contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contact = (): React.ReactElement => {
-  return <div>contact</div>;
-};
-
-export default Contact;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { Contact } from '../Pages/Contact';
 import { Create } from '../Pages/Create';
 import { Home } from '../Pages/Home';
 
 const Routes = (): React.ReactElement => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/contact" component={Contact} />
     <Route path="/create" component={Create} />
   </Switch>
 );
